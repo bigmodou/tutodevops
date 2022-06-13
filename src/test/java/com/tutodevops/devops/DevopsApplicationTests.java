cb8e0ccf101e3219e65cca62e34c228e2af4ccc4
modoudiop <modoudiop@gainde2000.sn>
2022-06-13 11:00:54 +0000

    first commit

---
+++ src/test/java/com/tutodevops/devops/DevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutodevops.devops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
